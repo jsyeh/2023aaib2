57248304e28d84d3bb235bd07cdfd5039af4fdd3
jsyeh <jsyeh.org@gmail.com>
2024-03-15 11:44:52 +0800

    week04

---
+++ week04/week04-4.cpp
@@ -0,0 +1,11 @@
+//Week04-4.cpp A008 9x9 table
+#include <stdio.h>
+int main()
+{
+	for(int i=1; i<10; i++){
+		for(int j=1; j<10; j++){
+			printf("%dx%d=%2d ",j, i, i*j);
+		}
+		printf("\n");
+	}
+}
